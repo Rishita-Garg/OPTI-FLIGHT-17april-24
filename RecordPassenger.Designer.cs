a36d53193828f0003b4b67f9f5bb9ecb583dd22f
Rishita-Garg <125175733+Rishita-Garg@users.noreply.github.com>
2024-04-17 20:47:38 +0530

    Add project files.

---
+++ RecordPassenger.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace OPTI_FLIGHT
+{
+    partial class RecordPassenger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.PassId = new System.Windows.Forms.TextBox();
+            this.NationalityCb = new System.Windows.Forms.ComboBox();
+            this.PassAd = new System.Windows.Forms.TextBox();
+            this.PhoneTb = new System.Windows.Forms.TextBox();
+            this.PassName = new System.Windows.Forms.TextBox();
+            this.PasssportTb = new System.Windows.Forms.TextBox();
+            this.GenderCb = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkViolet;
+            this.label1.Location = new System.Drawing.Point(219, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 34);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "OPTIFLIGHT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(101, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Passenger Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Passenger Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(101, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Passport Number";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(101, 183);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Passenger Address";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(101, 216);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Nationality";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(101, 253);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Gender";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(101, 291);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(38, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Phone";
+            // 
+            // PassId
+            // 
+            this.PassId.Location = new System.Drawing.Point(259, 83);
+            this.PassId.Name = "PassId";
+            this.PassId.Size = new System.Drawing.Size(100, 20);
+            this.PassId.TabIndex = 12;
+            // 
+            // NationalityCb
+            // 
+            this.NationalityCb.FormattingEnabled = true;
+            this.NationalityCb.Items.AddRange(new object[] {
+            "Indian",
+            "Chinease",
+            "US American",
+            "British",
+            "Spanish",
+            "Japanese",
+            "Korean",
+            "Russian",
+            "Ukranian",
+            "German"});
+            this.NationalityCb.Location = new System.Drawing.Point(259, 216);
+            this.NationalityCb.Name = "NationalityCb";
+            this.NationalityCb.Size = new System.Drawing.Size(121, 21);
+            this.NationalityCb.TabIndex = 13;
+            // 
+            // PassAd
+            // 
+            this.PassAd.Location = new System.Drawing.Point(259, 180);
+            this.PassAd.Name = "PassAd";
+            this.PassAd.Size = new System.Drawing.Size(100, 20);
+            this.PassAd.TabIndex = 14;
+            // 
+            // PhoneTb
+            // 
+            this.PhoneTb.Location = new System.Drawing.Point(259, 291);
+            this.PhoneTb.Name = "PhoneTb";
+            this.PhoneTb.Size = new System.Drawing.Size(100, 20);
+            this.PhoneTb.TabIndex = 15;
+            // 
+            // PassName
+            // 
+            this.PassName.Location = new System.Drawing.Point(259, 113);
+            this.PassName.Name = "PassName";
+            this.PassName.Size = new System.Drawing.Size(100, 20);
+            this.PassName.TabIndex = 16;
+            // 
+            // PasssportTb
+            // 
+            this.PasssportTb.Location = new System.Drawing.Point(259, 149);
+            this.PasssportTb.Name = "PasssportTb";
+            this.PasssportTb.Size = new System.Drawing.Size(100, 20);
+            this.PasssportTb.TabIndex = 17;
+            // 
+            // GenderCb
+            // 
+            this.GenderCb.FormattingEnabled = true;
+            this.GenderCb.Items.AddRange(new object[] {
+            "Female",
+            "Others",
+            "Male"});
+            this.GenderCb.Location = new System.Drawing.Point(259, 250);
+            this.GenderCb.Name = "GenderCb";
+            this.GenderCb.Size = new System.Drawing.Size(121, 21);
+            this.GenderCb.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(236, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Record";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(334, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(437, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(263, 402);
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(259, 364);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "View Passengers";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // RecordPassenger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 399);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GenderCb);
+            this.Controls.Add(this.PasssportTb);
+            this.Controls.Add(this.PassName);
+            this.Controls.Add(this.PhoneTb);
+            this.Controls.Add(this.PassAd);
+            this.Controls.Add(this.NationalityCb);
+            this.Controls.Add(this.PassId);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "RecordPassenger";
+            this.Text = "RecordPassenger";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox PassId;
+        private System.Windows.Forms.ComboBox NationalityCb;
+        private System.Windows.Forms.TextBox PassAd;
+        private System.Windows.Forms.TextBox PhoneTb;
+        private System.Windows.Forms.TextBox PassName;
+        private System.Windows.Forms.TextBox PasssportTb;
+        private System.Windows.Forms.ComboBox GenderCb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ ViewPassenger.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace OPTI_FLIGHT
+{
+    partial class ViewPassenger
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.GendCb = new System.Windows.Forms.ComboBox();
+            this.PpassTb = new System.Windows.Forms.TextBox();
+            this.PnameTb = new System.Windows.Forms.TextBox();
+            this.PaddTb = new System.Windows.Forms.TextBox();
+            this.natcb = new System.Windows.Forms.ComboBox();
+            this.PidTb = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PassengerDGV = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.PassengerDGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkViolet;
+            this.label1.Location = new System.Drawing.Point(245, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(190, 34);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "OPTIFLIGHT";
+            // 
+            // GendCb
+            // 
+            this.GendCb.FormattingEnabled = true;
+            this.GendCb.Location = new System.Drawing.Point(401, 150);
+            this.GendCb.Name = "GendCb";
+            this.GendCb.Size = new System.Drawing.Size(121, 21);
+            this.GendCb.TabIndex = 32;
+            // 
+            // PpassTb
+            // 
+            this.PpassTb.Location = new System.Drawing.Point(164, 153);
+            this.PpassTb.Name = "PpassTb";
+            this.PpassTb.Size = new System.Drawing.Size(100, 20);
+            this.PpassTb.TabIndex = 31;
+            // 
+            // PnameTb
+            // 
+            this.PnameTb.Location = new System.Drawing.Point(164, 117);
+            this.PnameTb.Name = "PnameTb";
+            this.PnameTb.Size = new System.Drawing.Size(100, 20);
+            this.PnameTb.TabIndex = 30;
+            // 
+            // PaddTb
+            // 
+            this.PaddTb.Location = new System.Drawing.Point(401, 88);
+            this.PaddTb.Multiline = true;
+            this.PaddTb.Name = "PaddTb";
+            this.PaddTb.Size = new System.Drawing.Size(100, 20);
+            this.PaddTb.TabIndex = 28;
+            // 
+            // natcb
+            // 
+            this.natcb.FormattingEnabled = true;
+            this.natcb.Location = new System.Drawing.Point(401, 117);
+            this.natcb.Name = "natcb";
+            this.natcb.Size = new System.Drawing.Size(121, 21);
+            this.natcb.TabIndex = 27;
+            // 
+            // PidTb
+            // 
+            this.PidTb.Location = new System.Drawing.Point(164, 81);
+            this.PidTb.Name = "PidTb";
+            this.PidTb.Size = new System.Drawing.Size(100, 20);
+            this.PidTb.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(297, 151);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Gender";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(297, 120);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Nationality";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(297, 88);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Passenger Address";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Passport Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Passenger Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Passenger Id";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(565, 184);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 36;
+            this.button4.Text = "BACK";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(565, 151);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 35;
+            this.button3.Text = "DELETE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(565, 117);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 34;
+            this.button2.Text = "UPDATE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(565, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 33;
+            this.button1.Text = "RESET";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PassengerDGV
+            // 
+            this.PassengerDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.PassengerDGV.Location = new System.Drawing.Point(0, 230);
+            this.PassengerDGV.Name = "PassengerDGV";
+            this.PassengerDGV.Size = new System.Drawing.Size(713, 218);
+            this.PassengerDGV.TabIndex = 37;
+            this.PassengerDGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.PassengerDGV_CellContentClick);
+            // 
+            // ViewPassenger
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 448);
+            this.Controls.Add(this.PassengerDGV);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.GendCb);
+            this.Controls.Add(this.PpassTb);
+            this.Controls.Add(this.PnameTb);
+            this.Controls.Add(this.PaddTb);
+            this.Controls.Add(this.natcb);
+            this.Controls.Add(this.PidTb);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ViewPassenger";
+            this.Text = "ViewPassenger";
+            this.Load += new System.EventHandler(this.ViewPassenger_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PassengerDGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox GendCb;
+        private System.Windows.Forms.TextBox PpassTb;
+        private System.Windows.Forms.TextBox PnameTb;
+        private System.Windows.Forms.TextBox PaddTb;
+        private System.Windows.Forms.ComboBox natcb;
+        private System.Windows.Forms.TextBox PidTb;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView PassengerDGV;
+    }
+}
